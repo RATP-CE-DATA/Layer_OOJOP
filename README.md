9af39c64b2ba776835e4644a583f28504171e3c6
Valérie Hanoka <valerie.hanoka@ratp.fr>
2024-06-27 09:59:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Layer_OOJOP
+
+Ce repo sert de serveur temporaire pour l'app OOJOP_PBI.
+
---
+++ reduce_geojson_size.sh
@@ -0,0 +1,14 @@
+
+
+echo "
+Ce script réduit à chaque fois la taille des geojson de 40%.
+Il est donc commenté pour éviter de réduire accidentellement les geojson
+une fois de trop. 
+Si vous souhaitez réduire la taille de certains geojson après mise à jour, 
+n'hésitez pas à l'adapter. Pour installer mapshaper: npm install -g mapshaper
+"
+
+# geojson_files=$(find . -type f -name "*.geojson")
+# for f in  $geojson_files; do
+#   mapshaper "$f" -simplify 40% -o "$f" format=geojson
+# done
