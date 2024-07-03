024a6404f13f2f879796030c405c5331349f3299
Valérie Hanoka <valerie.hanoka@ratp.fr>
2024-07-03 10:22:09 +0200

    remove decimals

--- reduce_geojson_size.sh
+++ reduce_geojson_size.sh
@@ -1,14 +1,12 @@
 
 
 echo "
-Ce script réduit à chaque fois la taille des geojson de 40%.
-Il est donc commenté pour éviter de réduire accidentellement les geojson
-une fois de trop. 
 Si vous souhaitez réduire la taille de certains geojson après mise à jour, 
-n'hésitez pas à l'adapter. Pour installer mapshaper: npm install -g mapshaper
+n'hésitez pas à l'adapter avec l'option "-simplify 40%".
+Pour installer mapshaper: npm install -g mapshaper
 "
 
-# geojson_files=$(find . -type f -name "*.geojson")
-# for f in  $geojson_files; do
-#   mapshaper "$f" -simplify 40% -o "$f" format=geojson precision=0.00001
-# done
+geojson_files=$(find . -type f -name "*.geojson")
+for f in  $geojson_files; do
+  mapshaper "$f" -o "$f" format=geojson precision=0.00001 force 
+done
